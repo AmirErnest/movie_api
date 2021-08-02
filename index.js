f81544edaa2cd039947c95e363b319c21a678667
Amir Ernest <amir.m.ernest@gmail.com>
2021-08-02 22:25:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const { check, validationResult } = require('express-validator');
 
 
 // GET List of Movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
